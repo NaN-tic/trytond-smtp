72e3c58874ad9ac549801d56a991f6c0d1590e6c
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:20:46 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part smtp module for Tryton.  The COPYRIGHT file at the top
-# level of this repository contains the full copyright notices and license
-# terms.
-try:
-    from trytond.modules.smtp.tests.test_smtp import suite
-except ImportError:
-    from .test_smtp import suite
-
-__all__ = ['suite']
